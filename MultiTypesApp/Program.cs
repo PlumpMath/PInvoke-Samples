dd246f5d83a33b3764b3823ae7866f0f43244891
Ariel Ben Horesh <ariel_bh@yahoo.com>
2014-11-17 19:06:13 +0200

    Step5: Adding PInvoke signatures.

--- MultiTypesApp/Program.cs
+++ MultiTypesApp/Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MultiTypesApp
 {
+
     static class Program
     {
+
+        #region PInvoke Methods (We will discuss this later! :))
+        [DllImport("kernel32", SetLastError = true)]
+        private static extern bool AttachConsole(int dwProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern bool FreeConsole();
+
+        [DllImport("kernel32.dll", EntryPoint = "AllocConsole", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern int AllocConsole();
+        #endregion
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
